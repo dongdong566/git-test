1b17ae8c34adb550a1347e265a7d29f6c4a83e4b
xdd <2595195362@qq.com>
2019-08-08 09:15:40 +0800

    本地工程添加到仓库

---
+++ git-test-project/src/com/xd/git/FirstClass.java
@@ -0,0 +1,14 @@
+package com.xd.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("555555555555555555");
+    }
+}
